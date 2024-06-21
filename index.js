46dc999f472d4ff3b43674705faa7d45c987d7eb
david <dla777@USERnoMacBook-Air.local>
2024-06-21 10:33:08 +0700

    update for performance

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
+const compression = require('compression');
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
+const router = express.Router();
+app.use(compression());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile( path.join( __dirname + "/public/index.html" ));
-});
 
-/* app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
- */
+// app.get('/', (req, res) => {
+//     res.sendFile( path.join( __dirname + "/public/index.html" ));
+// });
+
+
+
+
+
+
+router.get('/', function (req, reply) {
+    reply.sendFile(path.join(__dirname + '/public/index.html'));
+})
+
+app.get('/about', function (req, reply) {
+    reply.sendFile(path.join(__dirname + '/public/about.html'))
+})
+
+app.get('/garden-installation', function (req, reply) {
+    reply.sendFile(path.join(__dirname + '/public/garden-installation.html'))
+})
+
+app.get('/pavers', function (req, reply) {
+    reply.sendFile(path.join(__dirname + '/public/pavers.html'))
+})
+
+app.get('/deck-arbor', function (req, reply) {
+    reply.sendFile(path.join(__dirname + '/public/deck-arbor.html'))
+})
+
+app.get('/fence', function (req, reply) {
+    reply.sendFile(path.join(__dirname + '/public/fence.html'))
+})
+
+ app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+ 

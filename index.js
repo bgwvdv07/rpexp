4d444905eccff702095a8f3fea51002f4800abb1
David Aragon <davidaragon97@gmail.com>
2024-10-29 15:11:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/404', function (req, reply) {
 })
 
 app.get('/success', function (req, reply) {
-    reply.sendFile(path.join(__dirname + '/public/404.html'))
+    reply.sendFile(path.join(__dirname + '/public/success.html'))
 })
 
 

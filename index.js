48a2f5afa4dbf54c10a955f8f1d8d71b068ade40
USER <davidaragon97@gmail.com>
2024-10-29 15:58:38 +0800

    forms

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-app.use('/api/send-email', require('/public/js/send-email.js'))
+app.use('/api/send-email', require('/public/js/api/send-email.js'))
 
 
 
